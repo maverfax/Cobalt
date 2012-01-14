651636d57d38925ad0a39b0f8f219f5bca7678f2
Robert Herman <maverfax@gmail.com>
2012-01-13 23:39:16 -0600

    Documented core helpers

--- system/cobalt/functions.php
+++ system/cobalt/functions.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Returns an error message
+ *
+ * @param  string  $message
+ * @return void
+ */
 function error($message = '')
 {
 	include SYS_PATH . 'cobalt'.DS.'errors'.DS.'general.php';
@@ -7,22 +13,55 @@ function error($message = '')
 	exit;
 }
 
+/**
+ * Returns the total load time
+ *
+ * @param  int    $decimals
+ * @return double
+ */
 function load_time($decimals = 5)
 {
 	return round(microtime(TRUE) - COBALT_START, $decimals);
 }
 
+/**
+ * Returns the base url to the website
+ *
+ * @return string
+ */
 function base_url()
 {
 	return Request::base_url();
 }
 
+/**
+ * Returns the url to a page
+ *
+ * @return string
+ */
 function site_url($uri = '')
 {
-	return base_url() . 'index.php/' . $uri;
+	$page = '';
+
+	if(Config::get('index_page') != '')
+	{
+		$page = Config::get('index_page') . '/';
+	}
+
+	return base_url() . $page . $uri;
 }
 
+/**
+ * Redirects to a page and ends execution.
+ *
+ * @return void
+ */
 function redirect($uri = '')
 {
-	header('Location: ' . site_url($uri));
+	if( ! headers_sent())
+	{
+		header('Location: ' . site_url($uri));	
+	}
+
+	exit;
 }
